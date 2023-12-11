055c11943e9ae565f90c0742d0ffdb51a77ede3c
shouk0322 <shou.k.0322@gmail.com>
2023-12-12 02:16:36 +0900

    contactページ追加

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -2,24 +2,50 @@
   <div class="wrap">
     <Header />
     <main class="main">
-      <section class="contact">
-        <p class="contact-title">お問い合わせのページだよ</p>
-        <p class="contact-title">お問い合わせのページだよ</p>
+      <section class="p-contact">
+        <div class="p-contact__inner">
+          <h2 class="p-contact__title">お問い合わせ</h2>
+          <form action="" method="post">
+            <div class="p-contact__box">
+              <label for="name">お名前</label>
+              <input type="text" id="name" name="name" required />
+            </div>
+            <div class="p-contact__box">
+              <label for="company">企業名</label>
+              <input type="text" id="company" name="company" />
+            </div>
+            <div class="p-contact__box">
+              <label for="email">メールアドレス</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div class="p-contact__box">
+              <label for="phone">電話番号</label>
+              <input type="tel" id="phone" name="phone" />
+            </div>
+            <div class="p-contact__box">
+              <label for="inquiry">お問い合わせ内容</label>
+              <textarea id="inquiry" name="inquiry" required></textarea>
+            </div>
+            <div class="p-contact__submit">
+              <button type="submit">確認画面へ</button>
+            </div>
+          </form>
+        </div>
       </section>
-      <Footer />
+      <!-- <Footer /> -->
     </main>
   </div>
 </template>
 
 <script>
 import Header from "@/components/common/SideHeader.vue";
-import Footer from "@/components/common/Footer.vue";
+// import Footer from "@/components/common/Footer.vue";
 
 export default {
   name: "ContactPage",
   components: {
     Header,
-    Footer,
+    // Footer,
   },
 };
 </script>
@@ -27,13 +53,95 @@ export default {
 <style>
 .wrap {
   display: flex;
+  height: 100vh;
 }
+
 .main {
   width: 50%;
 }
-.contact {
+
+.p-contact {
   width: 100%;
-  height: 500px;
+  height: 100%;
   background-color: rgb(191, 242, 116);
+}
+
+.p-contact__inner {
+  max-width: 445px;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  padding: 40px 20px;
+}
+
+.p-contact__title {
+  font-size: 18px;
+  font-weight: bold;
+  display: inline-block;
+  margin-bottom: 30px;
+  position: relative;
+  z-index: 2;
+}
+.p-contact__title::before {
+  content: "CONTACT";
+  position: absolute;
+  right: -130px;
+  top: -13px;
+  font-size: 26px;
+  font-weight: bold;
+  color: #bfbebe;
+  letter-spacing: 0.05em;
+  z-index: -1;
+}
+
+.p-contact__submit {
+  text-align: center;
+}
+
+form {
+  padding: 0 10px;
+}
+
+input {
+  border: 1px solid;
+  width: 100%;
+  border-radius: 5px;
+}
+
+label {
+  display: block;
+  margin: 10px 0 5px;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="tel"],
+textarea {
+  width: 100%;
+  padding: 4px;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+textarea {
+  height: 300px;
+}
+
+button {
+  width: 250px;
+  padding: 2px;
+  background-color: #000;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  text-align: center;
+  margin: auto;
+  font-size: 14px;
+}
+
+button:hover {
+  background-color: #0056b3;
 }
 </style>
