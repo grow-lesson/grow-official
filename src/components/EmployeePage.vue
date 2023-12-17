bf051f36b9caa234ed525d5dca83e90c6ac3f4e9
matsunagatakuya <mt.salieri0709@gmail.com>
2023-12-18 01:54:53 +0900

    ページネーション用ボタン追加

--- src/components/EmployeePage.vue
+++ src/components/EmployeePage.vue
@@ -74,7 +74,27 @@
               </a>
             </li>
           </ul>
-
+          <div class="pageNation-box">
+            <ul class="pageNation-list">
+            <li class="pageNation-item">
+              <button class="pageNation-button">
+              <div class="pageNation-previous"></div>
+              </button>
+            </li>
+            <li class="pageNation-item">
+              <button class="pageNation-button">
+                <div class="pageNation-numberBox">
+                  <p class="pageNation-number">01</p>
+                </div>
+              </button>
+            </li>
+            <li class="pageNation-item">
+              <button class="pageNation-button">
+              <div class="pageNation-next"></div>
+              </button>
+            </li>
+          </ul>
+          </div>
         </div>
 
       </section>
@@ -208,6 +228,77 @@ bottom: 0;
   background-color: azure;
   padding: 20px;
   border-radius: 0 0 20px 20px;
+}
+
+.pageNation-box{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  margin-top: 60px;
+}
+
+.pageNation-list{
+  display: flex;
+}
+
+.pageNation-item{
+  background-color: azure;
+  width: 30px;
+  height: 30px;
+  border: solid 1px black;
+}
+
+.pageNation-item:nth-child(n+2){
+  margin-left: 15px;
+}
+
+.pageNation-previous {
+  position: relative;
+  display: inline-block;
+  padding: 10px;
+}
+
+.pageNation-numberBox{
+  width: 30px;
+
+}
+
+.pageNation-number{
+  text-align: center;
+  font-size: 13px;
+  font-weight: bold;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+.pageNation-previous::before {
+  content: '';
+  width: 10px;
+  height: 10px;
+  border-top: solid 2px #949494;
+  border-right: solid 2px #949494;
+  position: absolute;
+  left: 10px;
+  top: 8px;
+  transform: rotate(-135deg);
+}
+
+.pageNation-next {
+  position: relative;
+  display: inline-block;
+  padding: 10px;
+}
+
+.pageNation-next::before {
+  content: '';
+  width: 10px;
+  height: 10px;
+  border-top: solid 2px #949494;
+  border-right: solid 2px #949494;
+  position: absolute;
+  left: 8px;
+  top: 8px;
+  transform: rotate(45deg);
 }
 
 </style>
