d33ebe52310b0b326a1d554fbe9c571af800f4cc
kzyroot123 <kzy.root123@gmail.com>
2023-12-20 08:35:38 +0900

    hamburger-menu

--- src/components/common/SideHeader.vue
+++ src/components/common/SideHeader.vue
@@ -2,7 +2,7 @@
   <header class="header">
     <div class="header-container">
       <div class="header-logoWrap">
-      <img src="./../../assets/growlogo.png">
+        <img src="./../../assets/growlogo.png">
       </div>
       <nav class="header-nav">
         <ul class="header-list">
@@ -16,6 +16,20 @@
       </nav>
     </div>
   </header>
+  <div class="hamburger-menu">
+    <input type="checkbox" id="menu-btn-check">
+    <label for="menu-btn-check" class="menu-btn"><span></span></label>
+    <div class="menu-content">
+      <ul class="menu-list">
+        <li class="menu-item"><a @click="goToTopPage" class="header-link">Top</a></li>
+        <li class="menu-item"><a @click="goToPhilosophyPage" class="header-link">企業理念</a></li>
+        <li class="menu-item"><a @click="goToCompanyPage" class="header-link">事業内容</a></li>
+        <li class="menu-item"><a @click="goToEmployeePage" class="header-link">社員紹介</a></li>
+        <li class="menu-item"><a @click="goToAccomplishmentPage" class="header-link">案件実績</a></li>
+        <li class="menu-item"><a @click="goToContactPage" class="header-link">お問い合わせ</a></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -100,6 +114,7 @@ export default {
 .header {
   width: 555px;
 }
+
 .header-container {
   position: fixed;
   width: 555px;
@@ -107,19 +122,23 @@ export default {
   background-color: #2c3e50;
   padding: 80px 70px 0;
 }
+
 .header-logoWrap {
   width: 180px;
 }
-.header-logoWrap img{
+
+.header-logoWrap img {
   width: 100%;
 }
+
 .header-nav {
   margin-top: 40px;
 }
+
 .header-list {
   display: grid;
-  grid-template-rows: repeat(3,1fr);
-  grid-template-columns: repeat(2,1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   list-style: none;
   padding: 0;
   margin: 0;
@@ -135,6 +154,7 @@ export default {
   transition: color 0.3s ease;
   position: relative;
 }
+
 .header-item::after {
   position: absolute;
   right: 0px;
@@ -148,6 +168,115 @@ export default {
 /* メニューアイテムごとに余白を追加 */
 .header-item:not(:last-child) {
   margin-right: 15px;
+}
+
+.menu-btn {
+  position: fixed;
+  top: 0;
+  right: 5px;
+  display: flex;
+  height: 60px;
+  width: 60px;
+  justify-content: center;
+  align-items: center;
+  z-index: 90;
+}
+
+.menu-btn span,
+.menu-btn span:before,
+.menu-btn span:after {
+  content: '';
+  display: block;
+  height: 2px;
+  width: 25px;
+  border-radius: 2px;
+  background-color: #4d4d4d;
+  position: absolute;
+}
+
+.menu-btn span:before {
+  bottom: 8px;
+}
+
+.menu-btn span:after {
+  top: 8px;
+}
+
+
+#menu-btn-check:checked~.menu-btn span {
+  background-color: rgba(255, 255, 255, 0);
+  /*メニューオープン時は真ん中の線を透明にする*/
+}
+
+#menu-btn-check:checked~.menu-btn span::before {
+  bottom: 0;
+  transform: rotate(45deg);
+}
+
+#menu-btn-check:checked~.menu-btn span::after {
+  top: 0;
+  transform: rotate(-45deg);
+}
+
+#menu-btn-check {
+  display: none;
+}
+
+/* */
+.menu-content {
+  width: 50%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 100%;
+  /*leftの値を変更してメニューを画面外へ*/
+  z-index: 89;
+  background-color: #3584bb;
+  transition: all 0.5s;
+  /*アニメーション設定*/
+}
+
+.menu-content ul {
+  left: 50%;
+  padding: 70px 15px 0;
+}
+
+.menu-content ul li {
+  border-bottom: solid 1px #ffffff;
+  list-style: none;
+}
+
+.menu-content ul li a {
+  display: block;
+  width: 100%;
+  font-size: 15px;
+  box-sizing: border-box;
+  color: #ffffff;
+  text-decoration: none;
+  padding: 9px 15px 10px 0;
+  position: relative;
+}
+
+.menu-content ul li a::before {
+  content: "";
+  width: 7px;
+  height: 7px;
+  border-top: solid 2px #ffffff;
+  border-right: solid 2px #ffffff;
+  transform: rotate(45deg);
+  position: absolute;
+  right: 11px;
+  top: 16px;
+}
+
+#menu-btn-check:checked~.menu-content {
+  left: 50%;
+}
+
+@media (min-width: 1181px) {
+  .hamburger-menu {
+    display: none;
+  }
 }
 
 @media (max-width: 1180px) {
