8c0be8aff66045aea5151a39eb794127e404c82a
punba1228 <68808553+punba1228@users.noreply.github.com>
2023-12-21 22:56:00 +0900

    [change]配色適用、アニメーション付与

--- src/components/AccomplishmentPage.vue
+++ src/components/AccomplishmentPage.vue
@@ -3,7 +3,7 @@
     <Header />
     <main class="main">
       <section class="accomplishment">
-        <div class="heading">
+        <div class="heading fadeUp">
           <h2 class="heading-title">実績</h2>
           <div class="heading-engTitle">ACHIEVEMENTS</div>
         </div>
@@ -11,7 +11,7 @@
           <ul>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time01">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -24,7 +24,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time02">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -37,7 +37,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time03">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -50,7 +50,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time04">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -63,7 +63,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time05">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -76,7 +76,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time06">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -89,7 +89,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time07">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -102,7 +102,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time08">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -115,7 +115,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time09">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -128,7 +128,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time10">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -141,7 +141,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time11">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -154,7 +154,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time12">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -167,7 +167,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time13">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -180,7 +180,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time14">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -193,7 +193,7 @@
             </li>
             <li>
               <article>
-                <div class="accomplishment-flex">
+                <div class="accomplishment-flex fadeUp delay-time15">
                   <div class="accomplishment-img"></div>
                   <div class="accomplishment-block">
                     <p>2023.01</p>
@@ -228,17 +228,18 @@ export default {
 <style>
 .wrap {
   display: flex;
-  height: 100vh;
-  position: relative;
-  overflow: hidden;
 }
 .main {
-  overflow: scroll;
+}
+
+@media screen and (max-width: 767px) {
+  .main {
+    width: 100%;
+  }
 }
 .accomplishment {
-  width: 445px;
   position: relative;
-  background-color: aliceblue;
+  background-color: #f6f1f1;
   padding: 50px 10px;
 }
 .heading {
@@ -247,6 +248,7 @@ export default {
   padding: 24px 0;
 }
 .heading-title {
+  color: #4d4d4d;
   position: absolute;
   top: 35px;
   left: 20px;
@@ -254,6 +256,7 @@ export default {
   font-weight: bold;
 }
 .heading-engTitle {
+  color: #27a7cc;
   font-size: 32px;
   font-weight: bold;
   font-style: italic;
@@ -262,21 +265,97 @@ export default {
 .accomplishment-flex {
   display: flex;
   margin-top: 24px;
+  min-height: 150px;
 }
 .accomplishment-img {
-  width: 230px;
+  min-width: 230px;
   border: 1px #aaa solid;
-  background-color: white;
+  border-radius: 5px 0px 0px 5px;
+  background-color: #4d4d4d;
 }
 .accomplishment-block {
+  border: 1px #aaa solid;
+  border-radius: 0px 5px 5px 0px;
   padding: 15px 10px;
-  width: 300px;
-  background-color: gainsboro;
+  background-color: #f6f1f1;
 }
 .accomplishment-block p {
   font-size: 11px;
 }
 .accomplishment-block p:nth-child(2) {
   margin-top: 20px;
+}
+
+.fadeUp {
+  animation-name: fadeUpAnime;
+  animation-duration: 1s;
+  animation-fill-mode: forwards;
+}
+
+/* TODO：
+アニメーションの順出しをコンポーネント化で再現 */
+.delay-time01 {
+  animation-delay: 0.1s;
+}
+.delay-time02 {
+  animation-delay: 0.2s;
+}
+.delay-time03 {
+  animation-delay: 0.3s;
+}
+.delay-time04 {
+  animation-delay: 0.4s;
+}
+.delay-time05 {
+  animation-delay: 0.5s;
+}
+.delay-time06 {
+  animation-delay: 0.6s;
+}
+.delay-time07 {
+  animation-delay: 0.7s;
+}
+.delay-time08 {
+  animation-delay: 0.8s;
+}
+.delay-time09 {
+  animation-delay: 0.9s;
+}
+.delay-time10 {
+  animation-delay: 1s;
+}
+.delay-time11 {
+  animation-delay: 1.1s;
+}
+.delay-time12 {
+  animation-delay: 1.2s;
+}
+.delay-time13 {
+  animation-delay: 1.3s;
+}
+.delay-time14 {
+  animation-delay: 1.4s;
+}
+.delay-time15 {
+  animation-delay: 1.5s;
+}
+.delay-time16 {
+  animation-delay: 1.6s;
+}
+
+/* 
+TODO：
+フェードインアニメーション共通化
+ */
+@keyframes fadeUpAnime {
+  from {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 }
 </style>
