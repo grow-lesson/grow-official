620f0026ef75e8b4eb09ccaf9d092017ded68eb5
tamaru <on.101.tamaru.taichi@gmail.com>
2024-01-16 01:46:00 +0900

    引数を削除

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
@@ -44,7 +44,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  //history: createWebHistory("/grow-official/"),
+  history: createWebHistory("/"),
   routes,
 });
 
