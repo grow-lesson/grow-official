fef10f9482657a3ce9bc9000a034c2f716a80338
shouk0322 <shou.k.0322@gmail.com>
2024-03-01 21:09:13 +0900

    レイアウトの修正

--- src/components/CompanyPage.vue
+++ src/components/CompanyPage.vue
@@ -406,5 +406,12 @@ onMounted(() => {
     color: #fff;
     background-color: transparent !important;
   }
+
+  .demophoto img {
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+}
+
 }
 </style>
--- src/components/SuccessPage.vue
+++ src/components/SuccessPage.vue
@@ -2,11 +2,15 @@
   <div class="wrap">
     <Header />
     <main class="main">
-      <div>
-        <h2>お問い合わせが送信されました。</h2>
-        <p>ご連絡いただきありがとうございます。</p>
-        <router-link to="/">トップページに戻る</router-link>
-      </div>
+      <section class="success">
+        <div class="success-message wow animated fadeInUp">
+          <h1>ご連絡いただき、誠にありがとうございます！</h1>
+          <p>
+            お問い合わせいただいた内容を確認いたしました。<br />担当者が3営業日以内にご連絡を差し上げますので、今しばらくお待ちください。
+          </p>
+          <router-link class="btn" to="/">トップページに戻る</router-link>
+        </div>
+      </section>
       <Footer />
     </main>
   </div>
@@ -24,6 +28,12 @@ onMounted(() => {
 </script>
 
 <style>
+/* .bg {
+  position: fixed;
+    width: 555px;
+    height: 100vh;
+    background: linear-gradient(to bottom, #B0D3E1, #1B6D92);
+} */
 .wrap {
   display: flex;
 }
@@ -37,5 +47,64 @@ onMounted(() => {
     width: 100%;
   }
 }
+
+.success {
+  width: 100%;
+  min-height: calc(100vh - 50px);
+  padding-bottom: 50px;
+  background-color: #f6f1f1;
+  padding-top: 90px;
+}
+
+.success-message {
+  text-align: center;
+  padding: 0 50px;
+}
+
+.success-message h1 {
+  color: #333;
+  font-size: 24px;
+}
+
+.success-message p {
+  color: #666;
+  margin: 20px 0;
+}
+
+.success-message ul {
+  text-align: left;
+  list-style: none;
+  padding: 0;
+}
+
+.success-message li {
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 150px;
+  height: 35px;
+  padding: 10px;
+  background-color: #000;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  text-align: center;
+  margin: auto;
+  font-size: 14px;
+}
+@media screen and (max-width: 1180px) {
+ .success-message {
+  text-align: center;
+  padding: 0 20px;
+}
+
+.success-message h1 {
+  color: #333;
+  font-size: 20px;
+}
+}
+
 
 </style>
